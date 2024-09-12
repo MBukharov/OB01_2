5f527a78bf8fea333a7143f0518f03a72ed0f190
MBukharov <mikhail.bukharov@gmail.com>
2024-09-12 19:05:22 +0300

    ready

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from store import *
 
+#Создание магазинов
 store1 = Store("Первый магазин", "Казань")
+store2 = Store("Второй магазин", "Москва")
+store3 = Store("Третий магазин", "Владимир")
+
+#Добавление товаров в магазины
+store1.add_item("бананы", 100)
+store1.add_item("апельсины", 200)
+store1.add_item("мандарины", 300)
+
+store2.add_item("iphone 16", 150000)
+store2.add_item("iphone 15", 100000)
+store2.add_item("iphone 14", 80000)
+
+store3.add_item("Лыжи", 10000)
+store3.add_item("Санки", 1000)
+store3.add_item("Сноуборд", 30000)
+
+#Тест функций на примере третьего магазина
+print("\nТЕСТИРОВАНИЕ ФУНКЦИЙ")
+price = store3.get_item_price("Лыжи")                       #Получение цены товара
+print(f"Цена товара 'Лыжи': {price}")
+store3.update_item_price("Лыжи", 15000)  #Изменение цены товара
+price = store3.get_item_price("Лыжи")                       #Получение новой цены товара
+print(f"Цена товара 'Лыжи': {price}")
+store3.delete_item("Санки")                                 #Удаление товара
+store3.update_item_price("Санки", 2000)  #Попытка изменения цены удаленного товара
--- store.py
+++ store.py
@@ -8,15 +8,15 @@ class Store():
     #Добавление товара
     def add_item(self,item_name, item_price):
         self.items[item_name] = item_price
-        print(f"Товар {item_name} добавлен в магазин {self.name}.")
+        print(f"Товар {item_name} добавлен в магазин '{self.name}'.")
 
     #Удаление товара
     def delete_item(self,item_name):
         del self.items[item_name]
-        print(f"Товар {item_name} удален из магазина {self.name}.")
+        print(f"Товар {item_name} удален из магазина '{self.name}'.")
 
     #Получение цены
-    def show_item_price(self,item_name):
+    def get_item_price(self,item_name):
         try:
             return self.items[item_name]
         except KeyError:
@@ -28,4 +28,4 @@ class Store():
                 self.items[item_name] = new_price
                 print(f"Цена товара {item_name} обновлена")
             else:
-                print("Такого товара нет")
+                print(f"Товар {item_name} отсутствует магазине '{self.name}'")
